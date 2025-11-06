65dfdce3337ba50fcdc5ddc0a352ff2862cb66ed
DuyKhong94 <khongtrungduy12@gmail.com>
2025-11-06 22:14:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -205,8 +205,10 @@ with tab3:
     st.markdown("***Lưu ý nếu sau 30 phút vấn đề được giải quyết và đảm bảo những yếu tố cho line sản xuất hoạt động thì sản xuất phải khôi phục hoạt động và bỏ cảnh báo line down này đi.***")
     st.markdown("***Lưu ý nếu vẫn chưa ra nguyên nhân và nguy cơ cao ảnh hưởng chất lượng thì sản xuất sắp xếp nhân lực qua khu vực khác để giảm tổn thất cho nhà máy và bộ phận PC điều chỉnh kế hoạch nếu có thể.***")
 
+with tab4:
+
 with tab5:
-        
+
 
 
 
