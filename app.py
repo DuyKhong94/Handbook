1a005e8ab9eb999852b688dcc094be0fb4a348b1
DuyKhong94 <khongtrungduy12@gmail.com>
2025-11-06 22:19:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import random, string
 import os
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # ------------------ MongoDB ------------------
 def get_db():
@@ -211,6 +211,7 @@ with tab4:
     st.markdown("Hello I'm working on this tab")
 with tab5:
     st.subheader("Capability Analysis")
+
 
 
 
