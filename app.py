a8790c0713350e7800e2be971c47ae38042d8280
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-02 00:04:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -282,12 +282,16 @@ with tab6:
             df3=df2[df2['Curent line'] == selected_line]
             total_quantity = df3.groupby(['Curent line'], as_index=False)['QTY'].sum()
             total_quantity_sum= total_quantity['QTY'].sum()
-            st.markdown(f"Tổng số lượng:{int(total_quantity_sum)}")
+            total_verification_job=df3[df3['Job No'].str.startswith(('EB','QB','MP','PR'),na=False)]
+            total_verification_job_count=total_verification_job['Job No'].unique().count()
+            st.markdown(f"Tổng số lượng: {int(total_quantity_sum)}")
+            st.markdown(f"Tổng số job EB QB 1stMP: {int(total_verification_job_count)}")
             #st.dataframe(df2)
 with tab7:
    st.markdown("[1. Six Sigma Black Belt Handbook Third Edition](https://raw.githubusercontent.com/DuyKhong94/Handbook/90925edaa2a9c904df7d211e738daf0826aacee0/0.%20MUST%20READ_Hand%20Book%20Black.pdf)")
 
   
+
 
 
 
