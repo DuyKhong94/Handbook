3b68068ad9b714b666f2e2ad4ded47e448e89fae
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-02 17:23:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -261,6 +261,7 @@ with tab6:
             # Lấy bảng trái
             left_cols = ['TTI Model No', 'Job No', 'Curent line', 'Need Bulit QTY']
             st.write("Columns in df_raw:", df_raw.columns.tolist())
+            st.write("Columns in df_raw:", df_raw2.columns.tolist())
             df_left = df_raw[left_cols].dropna(how='all')
 
             # Lấy bảng phải
@@ -302,6 +303,7 @@ with tab7:
    st.markdown("[1. Six Sigma Black Belt Handbook Third Edition](https://raw.githubusercontent.com/DuyKhong94/Handbook/90925edaa2a9c904df7d211e738daf0826aacee0/0.%20MUST%20READ_Hand%20Book%20Black.pdf)")
 
   
+
 
 
 
