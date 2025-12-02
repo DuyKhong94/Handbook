f81a91867695bc1b019dabe540bad9590d2036f7
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-02 18:45:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ with tab6:
             st.divider()
             plt.figure(figsize=(10,5))
             bars=plt.barh(df5['Job No'],df5['Progress_Days'],left=df5['Cur Date'], color='skyblue')
-            for i, (bar, (_,row)) in enumerate (zip(bars,df_plot1.iterrows())):
+            for i, (bar, (_,row)) in enumerate (zip(bars,df5.iterrows())):
                 start=float(row['Cur Date'])
                 duration=float(row['Progress_Days'])
                 qty=row['Need Bulit QTY']
@@ -331,6 +331,7 @@ with tab7:
    st.markdown("[1. Six Sigma Black Belt Handbook Third Edition](https://raw.githubusercontent.com/DuyKhong94/Handbook/90925edaa2a9c904df7d211e738daf0826aacee0/0.%20MUST%20READ_Hand%20Book%20Black.pdf)")
 
   
+
 
 
 
