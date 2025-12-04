3124443baf26fce1f6ffdc57fdd25e8ad0ab8887
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-04 23:54:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -314,8 +314,8 @@ with tab6:
             total_line = len(df_filtered_by_date['Curent line'].unique())
             total_model = df_filtered_by_date['TTI Model No'].count()
             
-            st.markdown(f"**Tổng số line trong ngày: {total_line}**")
-            st.markdown(f"**Tổng số changeover trong ngày: {int(total_changeover)}**")
+            st.markdown(f"**Tổng số line trong ngày (Bao gồm line QB để theo dõi sự thay đổi): {total_line}**")
+            st.markdown(f"**Tổng số changeover trong ngày(Bao gồm line QB để theo dõi sự thay đổi): {int(total_changeover)}**")
             st.divider()
             df3 = df2[df2['Curent line'] == selected_line]
             df4=pd.read_excel(uploaded_file,sheet_name=selected_line,skiprows=3)
@@ -418,6 +418,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
