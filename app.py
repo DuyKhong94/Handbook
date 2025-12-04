0e52cca8fc88c199c278c488933f8adef8c57cf4
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-04 23:19:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ with tab6:
                                     )
             
             st.dataframe(df_filtered_by_date[['TTI Model No','Job No','Curent line']])   
-            total_changeover=df_filtered_by_date['Curent line'].unique()
+            total_changeover=len(df_filtered_by_date['Curent line'].unique())
             st.markdown(f"**Tổng số lượng changover trong ngày:{total_changeover}**")
             st.divider()
             df3 = df2[df2['Curent line'] == selected_line]
@@ -405,6 +405,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
