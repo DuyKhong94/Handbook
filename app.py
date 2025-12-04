11b15e7d82372ee8dad94aadddf94536cd45db71
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-04 22:33:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,17 @@ with tab6:
 
             # Loại bỏ các dòng hoàn toàn rỗng nếu có
             df2 = df2.dropna(how='all')
-
+            selected_date = st.date_input("Chọn ngày muốn xem job chạy:")
+            if selected_date:
+                day_start = pd.to_datetime(str(selected_date) + " 00:00:00")
+                day_end   = pd.to_datetime(str(selected_date) + " 23:59:59")
+            
+                df_filtered_by_date = df2[
+                    (df2['Cur Date'] <= day_end) &
+                    (df2['Completion date'] >= day_start)
+                ]
+            st.dataframe(df_filtered_by_date)
+            st.divider()
             # UI filter
             selected_line = st.selectbox("Chọn số line", df2['Curent line'].dropna().unique())
             df2['Cur Date']=pd.to_datetime(df2['Cur Date'],errors='coerce')
@@ -373,16 +383,7 @@ with tab6:
             st.pyplot(fig)
             st.divider()
             #st.dataframe(df2)
-            selected_date = st.date_input("Chọn ngày muốn xem job chạy:")
-            if selected_date:
-                day_start = pd.to_datetime(str(selected_date) + " 00:00:00")
-                day_end   = pd.to_datetime(str(selected_date) + " 23:59:59")
-            
-                df_filtered_by_date = df2[
-                    (df2['Cur Date'] <= day_end) &
-                    (df2['Completion date'] >= day_start)
-                ]
-            st.dataframe(df_filtered_by_date)
+
 with tab7:
     st.markdown("[1. Six Sigma Black Belt Handbook Third Edition - Source: American Society of Quality](https://raw.githubusercontent.com/DuyKhong94/Handbook/90925edaa2a9c904df7d211e738daf0826aacee0/0.%20MUST%20READ_Hand%20Book%20Black.pdf)")
     st.markdown("[2. Quy trình xử lý hàng lỗi trên line - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764746424/ROIPIE0016B_In-process_Reject_Operating_Instruction_4Mar.25_glp8ci.pdf)")
@@ -397,6 +398,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
