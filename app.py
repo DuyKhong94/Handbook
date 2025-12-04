345fcedd69d95a3338e6edfc4469f303c0f8bbc4
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-04 21:49:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -288,7 +288,7 @@ with tab6:
           
             st.divider()
 
-            df3 = df2[df2['Curent line'] == selected_line]
+            #df3 = df2[df2['Curent line'] == selected_line]
             df4=pd.read_excel(uploaded_file,sheet_name=selected_line,skiprows=3)
             quantity_col=['Job No','Need Bulit QTY']
             df_left2=df4[quantity_col].dropna(how='all')
@@ -395,6 +395,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
