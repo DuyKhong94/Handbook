f5a68301deb24d0082ac45967612b07be14ea6be
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-06 16:00:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ with tab6:
             #st.write(group_values)
             names=list(group_values.keys())
             values=list(group_values.values())
-            fig,ax=plt.subplots(figsize=(10, 5))
+            fig,ax=plt.subplots(figsize=(6, 3))
             bars=ax.bar(names, values, color="#90EE90")
             ax.set_xlabel("Khu Vực")
             ax.set_ylabel("Số lượng")
@@ -481,6 +481,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
