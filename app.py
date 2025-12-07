846eb42eb4b66d17925d4132b7a3330029d22ebc
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-07 09:30:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ with tab6:
                 line_type_map[line] = "QB AC"
             
             # Thêm cột Type vào df_line_model
-            df_line_model["Type"] = df_line_model["Curent line"].map(line_type_map).fillna("")
+            df_line_model["Category"] = df_line_model["Curent line"].map(line_type_map).fillna("")
             df_line_model["Changeover"] = df_line_model["Danh sách model"].apply(
                 lambda s: "YES" if len(s.split(", ")) > 1 else "NO"
             )
@@ -525,6 +525,7 @@ with tab7:
     st.markdown("[11. Tài liệu DOE - Author: Ni Nguyen ](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749441/27._DOE_-_RYOBI_slbk44.pdf)")
     st.markdown("[12. Tài liệu MSA GR&R - Author: Ni Nguyen](https://res.cloudinary.com/dij9ajlgm/image/upload/v1764749440/30_MSA_GRR_tn740z.pdf)")
    
+
 
 
 
