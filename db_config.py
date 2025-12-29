d1ea08c19cba7ef9bf254485d297f86b3cd0fc4f
DuyKhong94 <khongtrungduy12@gmail.com>
2025-12-29 10:07:00 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
 import os
-
+USERNAME= "duy1994_user"
+PASSWORD = urllib.parse.quote_plus("trungduy94")
+MONGO_URL="    f"mongodb+srv://{USERNAME}:{PASSWORD}"
+    "@duy1994.hou188l.mongodb.net/handbook"
+    "?retryWrites=true&w=majority"
 def get_db():
     # đường dẫn MongoDB của bạn
     url = os.getenv("MONGO_URL")
     client = MongoClient(url)
     db = client["handbook"]  # tên database
+
     return db
