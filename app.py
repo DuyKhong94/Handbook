a4a47d2a6237fcc0548c52211fc1437e81bc0a0c
DuyKhong94 <khongtrungduy12@gmail.com>
2026-01-03 12:46:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ with tab1:
 # ==========================================================
 with tab2:
     st.subheader("Tra cứu mã lỗi hoặc model")
-    with st.form("form_search_model_tab2")
+    with st.form("form_search_model_tab2"):
         search_model = st.text_input("Nhập model cần tra cứu:",key="search_model_tab2")
         submit_model= st.form_submit_button("🔍 Tra cứu Model")
     prefix=search_model[:6]
@@ -138,7 +138,7 @@ with tab2:
             
     st.divider()
 
-    with st.form("form_search_code_tab2")
+    with st.form("form_search_code_tab2"):
         search_code = st.text_input("Nhập mã lỗi cụ thể (VD: 333J12ABC):",key="search_code_tab2")
         submit_code= st.form_submit_button("🔍 Tra cứu lịch sử lỗi")
         
@@ -544,6 +544,7 @@ with tab7:
 
   
     
+
 
 
 
