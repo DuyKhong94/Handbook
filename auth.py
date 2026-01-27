ea3a344ada7d11bb1e9bf4ae8405c60411468a3a
DuyKhong94 <khongtrungduy12@gmail.com>
2026-01-27 14:42:24 +0700

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def require_login():
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in=False
+    
+    if not st.session_state.logged_in:
+        st.title("Login Process Engineering")
+        st.warning("Who are you? - Please contact Mr Duy Khong for access.")
+
+        username=st.text_input("User")
+        password=st.text_input("Password", type="password") 
+        if st.button("Login"):
+            if username=="peacbp" and password=="peacbpdeptrai":
+                st.session_state.logged_in=True
+                st.experimental_rerun()
+            else:
+                st.error("Invalid username or password")
+        st.stop()
