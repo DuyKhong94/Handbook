54e0a366bfc441216a1d8eb0742f2c550d9d8ddb
DuyKhong94 <khongtrungduy12@gmail.com>
2026-02-21 10:52:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -595,14 +595,14 @@ with tab8:
                 st.error("⚠️ Cần nhập đầy đủ thông tin!")
                 if result_upper <= result_lower:
                     st.error("Cần xem lại nhập đúng cận trên/cận dưới hay chưa")
-                    else: 
-                        st.write(f"🔼 Upper Limit: **{result_upper:.3f} mm**")
-                        st.write(f"🔽 Lower Limit: **{result_lower:.3f} mm**")
-                        st.info(
-                            f"📌 Nếu đo bằng **PIN GAUGE**:\n"
-                            f"- GO pin ≈ {result_lower - 0.01:.3f} mm\n"
-                            f"- NO-GO pin ≈ {result_upper + 0.01:.3f} mm"
-                        )
+                else: 
+                    st.write(f"🔼 Upper Limit: **{result_upper:.3f} mm**")
+                    st.write(f"🔽 Lower Limit: **{result_lower:.3f} mm**")
+                    st.info(
+                        f"📌 Nếu đo bằng **PIN GAUGE**:\n"
+                        f"- GO pin ≈ {result_lower - 0.01:.3f} mm\n"
+                        f"- NO-GO pin ≈ {result_upper + 0.01:.3f} mm"
+                    )
         
 
 
@@ -610,6 +610,7 @@ with tab8:
 
   
     
+
 
 
 
