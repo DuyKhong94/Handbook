6626c7cd6c6fd5f74234555ec648defde0783931
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-26 20:45:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ elif mode == "Chat Bot":
     
         with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
             response = client.chat.completions.create(
-                model="minimax/minimax-m2.5:free",
+                model="minimax/minimax-m2.5",
                 messages=st.session_state.messages
                 )
             reply = response.choices[0].message.content
