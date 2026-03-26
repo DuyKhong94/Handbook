fa38fb5f4f8a3480641dd3231131e18c125ad970
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-26 21:13:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ elif mode == "Chat Bot":
     
         with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
             response = client.chat.completions.create(
-                model="qwen/qwen3-next-80b-a3b-instruct:free",
+                model="z-ai/glm-4.5-air:free",
                 messages=st.session_state.messages
                 )
             reply = response.choices[0].message.content
