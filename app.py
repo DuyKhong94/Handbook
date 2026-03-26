66281b5a5bc83896350589e168f21d081155ba48
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-26 20:38:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ elif mode == "Chat Bot":
     
         with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
             response = client.chat.completions.create(
-                model="nvidia/llama-nemotron-embed-vl-1b-v2:free",
+                model="openai/sora-2-pro",
                 messages=st.session_state.messages
                 )
             reply = response.choices[0].message.content
