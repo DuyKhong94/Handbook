6616443bba1c51478ed429c8ce5a3e43f9a0e21c
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-26 11:53:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -616,12 +616,12 @@ with tab9:
     if "messages" not in st.session_state:
         st.session_state.messages = []
     
-    chat_container=st.container()
+  
     
-    with chat_container:
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
     
     if prompt := st.chat_input("you type here"):
         st.session_state.messages.append({"role": "user", "content": prompt})
