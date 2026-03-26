1161dfea367e946f2682b0e850acf0e666870a85
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-26 20:34:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ elif mode == "Chat Bot":
     
         with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
             response = client.chat.completions.create(
-                model="nvidia/nemotron-3-super-120b-a12b:free",
+                model="meta-llama/llama-3-8b-instruct:free",
                 messages=st.session_state.messages
                 )
             reply = response.choices[0].message.content
