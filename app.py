3630f4b8a99e6d6b2e3a235641b8f0426fb3ae0f
DuyKhong94 <khongtrungduy12@gmail.com>
2026-03-30 18:10:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -446,9 +446,29 @@ elif mode == "Chat Bot":
         # =====================================================
         # 🔥 CASE 2 + 3: AI RESPONSE
         # =====================================================
-        elif:
-            top_result = results[0] if results else None
 
+        elif not results:
+        
+            with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
+        
+                response = client.chat.completions.create(
+                    model="openai/gpt-4o-mini",
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are a helpful assistant. If the question is not related to defect data, answer normally."
+                        },
+                        {"role": "user", "content": prompt}
+                    ]
+                )
+        
+                reply = response.choices[0].message.content
+        
+                st.markdown("Thư ký AI: " + reply)
+        
+
+        else:
+            top_result = results[0]
             context = ""
             if top_result:
                 context = f"""
@@ -486,31 +506,7 @@ elif mode == "Chat Bot":
                     st.markdown(f"### 🔧 {top_result.get('error_code')}")
                     for img in top_result.get("images", []):
                         st.image(img)
-        else not results:
-        
-            with st.chat_message("assistant", avatar="https://raw.githubusercontent.com/DuyKhong94/Handbook/94fe8517a617d2b97cd20bd0ad834220d36b63f2/OIP.jpg"):
-        
-                response = client.chat.completions.create(
-                    model="openai/gpt-4o-mini",
-                    messages=[
-                        {
-                            "role": "system",
-                            "content": "You are a helpful assistant. If the question is not related to defect data, answer normally."
-                        },
-                        {"role": "user", "content": prompt}
-                    ]
-                )
-        
-                reply = response.choices[0].message.content
-        
-                st.markdown("Thư ký AI: " + reply)
-        
-            # 🔥 QUAN TRỌNG: lưu lại chat
-            st.session_state.messages.append({
-                "role": "assistant",
-                "content": reply
-            })
-        #st.session_state.messages.append({"role": "assistant", "content": reply})
+        st.session_state.messages.append({"role": "assistant", "content": reply})
 
     
 
