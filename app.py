9a10db073d3b02375868063291196ae87787bb64
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-07 14:37:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ elif mode == "🔥Trợ lý AI":
                 )
 
                 reply = response.choices[0].message.content
-                st.markdown("Thư ký AI: " + reply)
+                st.markdown("Emi Fukada: " + reply)
 
                 # ================= SHOW IMAGE =================
                 if top_result:
