88b78a83d89e7f15b5d26ea5fee36454fd386e36
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-09 11:58:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -534,7 +534,7 @@ elif mode == "📋Daily Pass Down":
         # Xóa dòng trống ở đầu
         lines=text.split("\n")
         if len(lines)>1:
-            indented_lines=[line[0]] + [f"   {line}" for line in lines[1:]]
+            indented_lines=[lines[0]] + [f"   {line}" for line in lines[1:]]
         else:
             indented_lines=lines
         result = "\n".join(indented_lines)
