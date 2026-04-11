7fe57acf23e6ed87c9516e600530005761019057
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-11 17:05:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -589,7 +589,7 @@ elif mode == "📈 Dashboard":
     df=pd.read_excel(linedown_lending_manhour_url,sheet_name="Quality & Downtime",skiprows=1)
     df1=df[df["Initial Owner"] == "PIE - Analysis"] # lọc theo PIE - Analysis
     df2=df1[df1["Confirm Owner"] =="PIE"]
-    acpk_models=["030","106","001597","001406","001606","001504","001997","011"]
+    acpk_models=["030","106","001597","001406","001606","001504","001997","011","095","096"]
     df3=df2[df2["Product"].astype(str).str.startswith(tuple(acpk_models))]
     sum_manhours=df3["Total Man. Hour"].sum()
     st.write(sum_manhours)
