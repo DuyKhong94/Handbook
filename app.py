c0a77aa98ad586a9f5a218b94c366b79da02a0ac
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 06:54:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -688,7 +688,7 @@ elif mode == "📈 Dashboard":
     "EOL Cases":[eol_count_chanh,eol_count_dien,eol_count_dinh,eol_count_phu]
     }
 
-    bars=axs[0,2].bar(df_eol["Name"],df_eol["EOL Cases"],color='skyblue',textsize=6)
+    bars=axs[0,2].bar(df_eol["Name"],df_eol["EOL Cases"],color='skyblue')
     axs[0, 2].tick_params(axis='x', labelsize=6)
     axs[0, 2].bar_label(bars, fontsize=6)
     axs[0,2].set_title("EOL Cases")
