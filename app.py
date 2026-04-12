82fb66ba299b88c271db1f02962308c06d6c84e0
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-12 22:50:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -623,7 +623,7 @@ elif mode == "📈 Dashboard":
         x=bar.get_x() + bar.get_width()/2
         axs[0,0].text(x,height,f"{height:.2f}",ha='center',va='bottom',fontsize=6)
 
-    wedges, texts = ax.pie(df_pie["Value"], startangle=90)
+    wedges, texts = axs[0,1].pie(df_pie["Value"], startangle=90)
     for i, (p, val, area) in enumerate(zip(wedges, df_pie["Value"], df_pie["Area"])):
         ang = (p.theta2 - p.theta1)/2. + p.theta1
         y = np.sin(np.deg2rad(ang))
