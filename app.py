4054d648628500f8fd7dbbd1ff2efb0d51e58e80
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-12 22:57:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -651,7 +651,7 @@ elif mode == "📈 Dashboard":
     centre_circle = plt.Circle((0, 0), 0.5, fc='white')
     fig.gca().add_artist(centre_circle)
         
-    #axs[0,1].set_title("PE Line Down Contributors")
+    axs[0,1].set_title("Line Down Contributors")
     plt.tight_layout()
     st.pyplot(fig)
 
