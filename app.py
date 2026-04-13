4cb613d45232a09867694cf0ac1b74b2c12c03dd
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 15:19:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ elif mode == "📈 Dashboard":
 
         
     #PIE CHART
-    wedges, texts = axs[0,1].pie(df_pie["Value"], startangle=90)
+    wedges, texts = axs[0,1].pie(df_pie["Value"], startangle=90,radius=0.8)
     for i, (p, val, area) in enumerate(zip(wedges, df_pie["Value"], df_pie["Area"])):
         ang = (p.theta2 - p.theta1)/2. + p.theta1
         y = np.sin(np.deg2rad(ang))
