1dde968ea11d427a14941a2cec7674d7c928a23f
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 07:39:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -700,7 +700,7 @@ elif mode == "📈 Dashboard":
     axs[0, 2].bar_label(bars, fontsize=6)
     axs[0,2].set_title("EOL Cases")
     for bar in bars:
-        height=bar.get_height()
+        height=bar.get_height()*0.99
         xtext=bar.get_x() + bar.get_width()/2
         axs[0,2].text(xtext,height,f"{height}",ha='center',va='bottom',fontsize=6)
     
