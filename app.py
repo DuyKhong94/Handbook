a5066033c591a3b23816073a86bccb24685daa11
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 15:09:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -709,8 +709,8 @@ elif mode == "📈 Dashboard":
     #KAIZEN
     df_kaizen={
     "Name":["Chánh","Điền","Định","Phú"],
-    "Kaizen":[0, 2, 0, 0]}
-    bars=axs[1,1].bar(df_kaizen["Name"],str(df_kaizen["Kaizen"]),color='red')
+    "Kaizen":str([0, 2, 0, 0])}
+    bars=axs[1,1].bar(df_kaizen["Name"],df_kaizen["Kaizen"],color='red')
     axs[1,1].tick_params(axis='x', labelsize=6)
     for bar in bars:
         height=bar.get_height()
