d996250972fb5a9a9853c74ccb5feb7a89acc942
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 18:49:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -668,7 +668,7 @@ elif mode == "📈 Dashboard":
         "Value":[sum_manhours_acpk_usd,sum_manhours_dc_usd,sum_manhours_eec_usd],
         "Value2":[sum_manhours_acpk,sum_manhours_dc,sum_manhours_eec]})
         
-    fig, axs = plt.subplots(2,2, figsize=(10, 5))
+    fig, axs = plt.subplots(2,3, figsize=(10, 5))
     bars=axs[0,0].bar(df_pie["Area"],df_pie["Value2"],color='green')
     axs[0,0].tick_params(axis='x', labelsize=8)
     #axs[0].bar_label(bars, fontsize=8,padding=3)
