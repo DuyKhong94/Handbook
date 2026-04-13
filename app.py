33f72bd7d6bebc8db11b580c95e5a61fd22f36c5
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 19:02:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -774,7 +774,7 @@ elif mode == "📈 Dashboard":
     axs[0,2].set_ylim(0, max(df_kaizen["Kaizen"]) + 1)
 
     #Main Factor
-    material=len(df7[df7["5M1D"] =="Material Issue"])
+    material=len(df7[df7["5M1D"] =="Material issue"])
 
     st.write(material)
     plt.tight_layout()
