9d1fa4a3ae3eb6406c3b20b844337b2519f9a9cd
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-13 11:15:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -657,7 +657,7 @@ elif mode == "📈 Dashboard":
     centre_circle = plt.Circle((0, 0), 0.5, fc='white')
     fig.gca().add_artist(centre_circle)
         
-    axs[0,1].set_title("Line Down Contributors",y=1.2)
+    axs[0,1].set_title("Line Down Contributors",y=1.17)
 
 
 
