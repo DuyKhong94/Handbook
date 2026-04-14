a5d89d26b1a757aab42ac75e69ada71039f7fbdc
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-14 08:48:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -711,7 +711,7 @@ elif mode == "📈 Dashboard":
     centre_circle = plt.Circle((0, 0), 0.5, fc='white')
     fig.gca().add_artist(centre_circle)
         
-    axs[0,1].legend(df_pie["Area"],loc='lower right',bbox_to_anchor=(1.8, 0),title="Dollars Cont.")
+    axs[0,1].legend(df_pie["Area"],loc='lower right',bbox_to_anchor=(1.65, 0),title="Dollars Cont.")
     
 
 
