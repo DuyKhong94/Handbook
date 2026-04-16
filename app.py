323b13b5178c350c5116d2d43b5abfc556a4606d
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-16 19:50:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -858,9 +858,9 @@ elif mode == "📈 Dashboard":
 
     df1c=df1[df1["Product"].astype(str).str.startswith(tuple(eec_models))]
 
-    dfc1=dfa[dfa["Product"].astype(str).str.startswith(tuple(eec_models))]# PDN nhap qua cho SQE
-    eec_linedown_resolved=(dfc1["Total Man. Hour"]).sum() + (df1c["Total Man. Hour"]).sum()
-
+    #dfc1=dfa[dfa["Product"].astype(str).str.startswith(tuple(eec_models))]# PDN nhap qua cho SQE
+    #eec_linedown_resolved=(dfc1["Total Man. Hour"]).sum() + (df1c["Total Man. Hour"]).sum()
+    eec_linedown_resolved=(df1c["Total Man. Hour"]).sum()
     st.write(acpk_linedown_resolved)
     st.write(eec_linedown_resolved)
     plt.tight_layout()
