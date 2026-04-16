b3ea22991aaec273fbaa427fc081c71efce85ed8
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-16 18:12:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -819,7 +819,12 @@ elif mode == "📈 Dashboard":
     axs[1,1].tick_params(axis='x', labelsize=6,labelrotation=90)
     
     ## material factors
-
+    factors=df7["Material Category"].unique()
+    st.write(factors)
+    metal=len(df7["Material Category"]=="Metal")
+    metal=len(df7["Material Category"]=="Metal")
+    metal=len(df7["Material Category"]=="Metal")
+    metal=len(df7["Material Category"]=="Metal")
     metal=len(df7["Material Category"]=="Metal")
     st.write(metal)
     
