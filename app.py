f4d37c44ce98f6f930951724f2710d91e3e2beb1
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-16 22:25:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -885,7 +885,7 @@ elif mode == "📈 Dashboard":
     
     df_count = df7a.groupby("Date").size()
     
-    axs[2,1].plot(df_count.index, df_count.values, marker='o')
+    axs[2,0].plot(df_count.index, df_count.values, marker='o')
     plt.tight_layout()
     st.pyplot(fig)
 
