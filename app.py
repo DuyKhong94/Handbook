f2ed1bd8519ee732e92c94497463522b74849eee
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-16 22:44:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -899,7 +899,7 @@ elif mode == "📈 Dashboard":
     )
     
     axs[2,0].plot(df_count.index, df_count.values,color="#2a9d8f")
-    axs[2,0].xaxis.set_major_locator(mdates.DayLocator(interval=3))   # 👈 mỗi 3 ngày
+    axs[2,0].xaxis.set_major_locator(mdates.DayLocator(interval=1))   # 👈 mỗi 3 ngày
     axs[2,0].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
     axs[2,0].set_ylim(0,10)
     axs[2,0].tick_params(axis='x',labelsize=5)
