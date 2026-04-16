a9ed7acdf00b494f7bbb829143b3b405bcd7454a
DuyKhong94 <khongtrungduy12@gmail.com>
2026-04-16 22:31:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -888,7 +888,7 @@ elif mode == "📈 Dashboard":
     
     axs[2,0].plot(df_count.index, df_count.values)
     axs[2,0].xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))  # chỉ hiện MM-DD
-    axs[2,1].set_ylim(0,10)
+    axs[2,0].set_ylim(0,10)
     axs[2,0].tick_params(axis='x',labelsize=6)
     plt.tight_layout()
     st.pyplot(fig)
