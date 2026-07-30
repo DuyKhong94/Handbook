333cc0096278925afeb6efd915cfec7e83c445de
DuyKhong94 <khongtrungduy12@gmail.com>
2026-07-30 11:33:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -864,8 +864,8 @@ elif mode == "📈 Dashboard":
     dfa1=dfa[dfa["Product"].astype(str).str.startswith(tuple(acpk_models))]# PDN nhap qua cho SQE
     
     df1a=df1[df1["Product"].astype(str).str.startswith(tuple(acpk_models))]
-    st.dataframe(df1a)
-    st.dataframe(dfa1)
+    #st.dataframe(df1a)
+    #st.dataframe(dfa1)
     acpk_linedown_resolved=(dfa1["Total Man. Hour"]).sum() + (df1a["Total Man. Hour"]).sum()
     #df1b=df1[df1["Product"].astype(str).str.startswith(tuple(dc_models))]
     #dc_linedown_resolved=(df1b["Total Man. Hour"]).sum()
@@ -875,7 +875,7 @@ elif mode == "📈 Dashboard":
     #eec_linedown_resolved=(dfc1["Total Man. Hour"]).sum() + (df1c["Total Man. Hour"]).sum()
     eec_linedown_resolved=(df1c["Total Man. Hour"]).sum()
     total_linedown=acpk_linedown_resolved + eec_linedown_resolved
-    st.write(acpk_linedown_resolved)
+    #st.write(acpk_linedown_resolved)
     # st.write(eec_linedown_resolved)
     #st.write(dc_linedown_resolved)
     df_linedown_resolved={
