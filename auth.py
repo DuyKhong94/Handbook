1abe7fac198b4c186358f2103ed3f3ccfec416d5
DuyKhong94 <khongtrungduy12@gmail.com>
2026-08-23 09:06:44 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,22 +1,89 @@
 import streamlit as st
 
-def require_login():
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in=False
-    
-    if not st.session_state.logged_in:
-        st.title("Login Process Engineering App")
-        st.warning("Hold on 😅! Who are you? - Please contact Mr Duy Khong for access.")
 
-        username=st.text_input("User")
-        password=st.text_input("Password", type="password") 
+
+
+
+USERS = {
+
+    "peacbp": {
+        "password": "peacbpdeptrai",
+        "permissions": [
+
+
+            "analysis",
+            "erp",
+            "team_center",
+  
+
+            "ai",
+            "daily_passdown"
+        ]
+    },
+
+    "user01": {
+        "password": "123456",
+        "permissions": [
+            "ai"
+        ]
+    },
+
+    "admin": {
+        "password": "trungduy94",
+        "permissions": [
+            "dashboard",
+            "add_error",
+            "analysis",
+            "erp",
+            "team_center",
+            "reference",
+            "spreadsheet",
+            "ai",
+            "daily_passdown"
+        ]
+    }
+}
+def require_login():
+
+    if "logged_in" not in st.session_state:
+        st.session_state.logged_in = False
+
+    if not st.session_state.logged_in:
+
+        st.title("Login Process Engineering App")
+
+        st.warning(
+            "Hold on 😅! Who are you? "
+            "- Please contact Mr Duy Khong for access."
+        )
+
+        username = st.text_input("User")
+
+        password = st.text_input(
+            "Password",
+            type="password"
+        )
+
         if st.button("Login"):
-            if username=="peacbp" and password=="peacbpdeptrai":
-                st.session_state.logged_in=True
+
+            user = USERS.get(username)
+
+            if user and user["password"] == password:
+
+                st.session_state.logged_in = True
+
+                # Lưu username
+                st.session_state.username = username
+
+                # Lưu quyền
+                st.session_state.permissions = user["permissions"]
+
                 st.rerun()
+
             else:
-                st.error("Invalid username or password")
+
+                st.error(
+                    "Invalid username or password"
+                )
 
         st.stop()
-
-
