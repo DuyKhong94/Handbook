14f1603ed334ffafe376dd562e87b76c0aa7054d
DuyKhong94 <khongtrungduy12@gmail.com>
2026-08-23 11:20:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,9 @@ st.markdown("""
 <style>
 /* Cố định độ rộng Sidebar */
 section[data-testid="stSidebar"] {
-    width: 335px !important;
-    min-width: 335px !important;
-    max-width: 335px !important;
+    width: 345px !important;
+    min-width: 345px !important;
+    max-width: 345px !important;
 }
 
 /* Tắt thanh kéo resize */
