d1ba39e73ae70edaa75c73e044928898df6747d8
DuyKhong94 <khongtrungduy12@gmail.com>
2026-08-23 11:17:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -53,9 +53,9 @@ st.markdown("""
 <style>
 /* Cố định độ rộng Sidebar */
 section[data-testid="stSidebar"] {
-    width: 300px !important;
-    min-width: 300px !important;
-    max-width: 300px !important;
+    width: 350px !important;
+    min-width: 350px !important;
+    max-width: 350px !important;
 }
 
 /* Tắt thanh kéo resize */
