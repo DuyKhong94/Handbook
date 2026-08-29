3b5acd9b5499923f351e98c50f3787087eddace8
DuyKhong94 <khongtrungduy12@gmail.com>
2026-08-29 12:02:29 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -81,7 +81,7 @@ def require_login():
             else:
 
                 st.error(
-                    "Your password is invalid or Your account is expired\nPlease contact IT support!"
+                    "Your password is invalid or Your account is expired\n Please contact IT support!"
                 )
 
         st.stop()
